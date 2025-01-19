8b79f9d4429379db4115090554d389156e00bc37
Lida Yañez <ligra_20@outlook.com>
2025-01-19 17:09:25 -0500

    Desarrollo del funcionamiento y detalle del funcionamiento

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-## Proyecto Sorteo Amigo Secreto
+## Sorteo Amigo Secreto
+La aplicación a continuación sirve para realizar sorteos en base a una lista de nombres.
+
+# Funcionamiento
+* Digitar un nombre donde se visualiza el mensaje de **"Escribe un nombre"**
+* Luego presionar el botón de **"Añadir"**
+* La lista de nombres que se van agregando se van a visualizar en la parte superior al botón que dice **"Sortear amigo"**
+* Una vez que la lista este completa presionar el botón **"Sortear amigo"**, saldra un mensaje de color verde indicando **"El amigo secreto sorteado es: ...."**
--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let elementoAmigo = document.getElementById('amigo');
+    let nombreAmigo = elementoAmigo.value.trim(); //Limpiar espacios en blanco
+    
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    elementoAmigo.value = ''; //Dejar el input vacío 
+    
+    mostrarAmigos();
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function mostrarAmigos() {
+    let amigosLista = document.getElementById('listaAmigos');
+    amigosLista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        amigosLista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    let amigoSorteado = document.getElementById('resultado');
+    let totalAmigos = amigos.length;
+    let indiceAmigoSorteado = Math.floor(Math.random() * totalAmigos);
+
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    document.getElementById('listaAmigos').innerHTML = '';
+    amigoSorteado.innerHTML = `El amigo secreto sorteado es: ${amigos[indiceAmigoSorteado]}`;
+    amigos=[];
+}
+
